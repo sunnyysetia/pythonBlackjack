df2d9b9488678bfbf52976a32f6ea71493c67b44
Sunny Setia <sunny@Sunnys-MacBook-Pro-2.local>
2023-01-31 23:00:21 +1300

    using person class to deal intial cards

--- main.py
+++ main.py
@@ -34,36 +34,57 @@ def printCards(deck):
     for card in deck:
         printCard(card.rank,card.suit)
 
-def dealCard(who, deck):
+def dealCard(person, deck):
     no_of_cards_remaining = len(deck) -1
     randomIndex = random.randint(0, no_of_cards_remaining)
-    who.append(deck[randomIndex])
+
+    # Apend card to person's cards
+    person.cards.append(deck[randomIndex])
+
+    # Append person's value attribute
+    rank = deck[randomIndex].rank
+    if rank == 'A':
+        if person.val <= 10:
+            person.val += 11
+        else:
+            person.val += 1
+    elif rank == 'J' or rank == 'Q' or rank == 'K':
+        person.val += 10
+    else:
+        person.val += int(rank)
+
+    # Remove card from deck
     del deck[randomIndex]
 
-
-
+    
+    
+class person:
+    def __init__(self):
+        self.cards = []
+        self.val = 0
 
 def blackjack():
     # Create new deck
     deck = createDeck()
 
     # Initialise dealer and player cards
-    dealerCards=[]
-    playerCards=[]
 
-    dealCard(dealerCards,deck)
-    dealCard(dealerCards,deck)
-    dealCard(playerCards,deck)
+    dealer = person()
+    player = person()
+
+    dealCard(dealer,deck)
+    dealCard(dealer,deck)
+    dealCard(player,deck)
 
     print("Dealer's Cards")
-    printCards(dealerCards)
+    print(dealer.val)
+    printCards(dealer.cards)
 
     print("Player's Cards")
-    printCards(playerCards)
+    print(player.val)
+    printCards(player.cards)
 
 
 
-# deck = createDeck()
 
-# print(len(deck))
 blackjack()
